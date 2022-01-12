871f6beb7c2150c32ec840360c2c98b185394b59
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-12 14:19:31 +0000

    Test

--- scripts/MainSceneController.cs
+++ scripts/MainSceneController.cs
@@ -6,11 +6,6 @@ public class MainSceneController : Node2D
 	public override void _Ready()
 	{
 
-		Sprite sprite = GetNode<Sprite>("");
-		AnimatedSprite animatedSprite = GetNode<AnimatedSprite>("");
-
-		animatedSprite.Play("");
-
 	}
 
 }
