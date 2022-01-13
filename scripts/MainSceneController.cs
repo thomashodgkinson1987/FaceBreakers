adb5567b29bf4460db4c8fec9f29065db00ba21b
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-13 00:28:52 +0000

    Initial commit

--- scripts/MainSceneController.cs
+++ scripts/MainSceneController.cs
@@ -3,9 +3,20 @@ using Godot;
 public class MainSceneController : Node2D
 {
 
+	private Sprite sprite;
+
 	public override void _Ready()
 	{
+		sprite = GetNode<Sprite>("Sprite");
+	}
 
+	public override void _Process(float delta)
+	{
+		sprite.Position += Vector2.Right * delta;
+
+		int bob = 1;
+
+		f;
 	}
 
 }
