ed84e033ba50b8100f511191e428f2a3c6636944
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-13 00:15:35 +0000

    Test

--- scripts/MainSceneController.cs
+++ scripts/MainSceneController.cs
@@ -3,9 +3,17 @@ using Godot;
 public class MainSceneController : Node2D
 {
 
+	private float m_speed = 10f;
+	private Sprite m_sprite;
+
 	public override void _Ready()
 	{
+		m_sprite = GetNode<Sprite>("Sprite");
+	}
 
+	public override void _Process(float delta)
+	{
+		m_sprite.Position += Vector2.Right * m_speed * delta;
 	}
 
 }
