5980826cc49cf151033ada679aa91dd47c5f1ab7
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-14 18:41:27 +0000

    Added player controls

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,12 +3,104 @@ using Godot;
 public class Player : Node2D
 {
 
+	#region Nodes
+
 	private Sprite node_sprite;
+	private Area2D node_area2D;
+	private CollisionShape2D node_collisionShape2D;
+
+	#endregion // Nodes
+
+
+
+	#region Properties
+
+	[Export] public float MoveSpeed { get; set; } = 16f;
+
+	public Vector2 Input_Direction { get; set; } = Vector2.Zero;
+
+	public bool IsInputJustPressed_Shoot { get; set; } = false;
+	public bool IsInputPressed_Shoot { get; set; } = false;
+	public bool IsInputJustReleased_Shoot { get; set; } = false;
+
+	#endregion // Properties
+
+
+
+	#region Godot methods
 
 	public override void _Ready()
 	{
 		node_sprite = GetNode<Sprite>("Sprite");
+		node_area2D = GetNode<Area2D>("Area2D");
+		node_collisionShape2D = node_area2D.GetNode<CollisionShape2D>("CollisionShape2D");
 	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		HandleInput(delta);
+	}
+
+	public override void _Process(float delta)
+	{
+		UpdateInput();
+	}
+
+	#endregion // Godot methods
+
+
+
+	#region Private methods
+
+	private void HandleInput(float delta)
+	{
+		HandleInput_Direction(delta);
+		HandleInput_Shoot();
+	}
+
+	private void HandleInput_Direction(float delta)
+	{
+		Vector2 position = Position;
+
+		position += Input_Direction * MoveSpeed * delta;
+
+		Position = position;
+	}
+
+	private void HandleInput_Shoot()
+	{
+		if (IsInputJustPressed_Shoot)
+		{
+			GD.Print("Player shoot");
+		}
+	}
+
+	private void UpdateInput()
+	{
+		UpdateInput_Direction();
+		UpdateInput_Shoot();
+	}
+
+	private void UpdateInput_Direction()
+	{
+		Vector2 input_direction = Vector2.Zero;
+
+		if (Input.IsActionPressed("player_move_left")) input_direction.x -= 1;
+		if (Input.IsActionPressed("player_move_right")) input_direction.x += 1;
+		if (Input.IsActionPressed("player_move_up")) input_direction.y -= 1;
+		if (Input.IsActionPressed("player_move_down")) input_direction.y += 1;
+
+		Input_Direction = input_direction;
+	}
+
+	private void UpdateInput_Shoot()
+	{
+		IsInputJustPressed_Shoot = Input.IsActionJustPressed("player_shoot");
+		IsInputPressed_Shoot = Input.IsActionPressed("player_shoot");
+		IsInputJustReleased_Shoot = Input.IsActionJustReleased("player_shoot");
+	}
+
+	#endregion // Private methods
 
 }
 
