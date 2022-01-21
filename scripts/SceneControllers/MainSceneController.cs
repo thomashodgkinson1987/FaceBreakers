c1f404af66a5816078ffd277130f531224c02e1a
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-21 13:51:07 +0000

    Bug fixes to player hitbox name
    Added enemy projectile spawn testing

--- scripts/SceneControllers/MainSceneController.cs
+++ scripts/SceneControllers/MainSceneController.cs
@@ -8,11 +8,17 @@ public class MainSceneController : Node2D
 	private GameHUDController node_gameHUDController;
 	private Player node_player;
 
+	private Node2D node_enemyProjectiles;
+	private Position2D node_enemyProjectileSpawnPosition;
+	private Timer node_enemyProjectileSpawnTimer;
+
 	#endregion // Nodes
 
 
 
 	#region Fields
+
+	[Export] private PackedScene m_enemyProjectilePackedScene;
 
 	private int _lives = 3;
 	private int _score = 0;
@@ -53,6 +59,9 @@ public class MainSceneController : Node2D
 	{
 		node_gameHUDController = GetNode<GameHUDController>("GameHUD");
 		node_player = GetNode<Player>("Player");
+		node_enemyProjectiles = GetNode<Node2D>("EnemyProjectiles");
+		node_enemyProjectileSpawnPosition = GetNode<Position2D>("EnemyProjectileSpawnPosition");
+		node_enemyProjectileSpawnTimer = GetNode<Timer>("EnemyProjectileSpawnTimer");
 	}
 
 	public override void _Ready()
@@ -62,6 +71,31 @@ public class MainSceneController : Node2D
 	}
 
 	#endregion // Godot methods
+
+
+
+	#region Public methods
+
+	public void FireEnemyProjectile()
+	{
+		Projectile projectile = m_enemyProjectilePackedScene.Instance<Projectile>();
+		node_enemyProjectiles.AddChild(projectile);
+		projectile.GlobalPosition = node_enemyProjectileSpawnPosition.GlobalPosition;
+		projectile.Rotation = node_enemyProjectileSpawnPosition.Rotation;
+	}
+
+	#endregion // Public methods
+
+
+
+	#region Private methods
+
+	private void OnEnemyProjectileSpawnTimerTimeout()
+	{
+		FireEnemyProjectile();
+	}
+
+	#endregion // Private methods
 
 }
 
