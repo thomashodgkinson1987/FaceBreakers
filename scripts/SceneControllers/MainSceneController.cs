0b7256067c055747248c0b8d2fcd0980a2faf033
Thomas Hodgkinson <thomashodgkinson1987@gmail.com>
2022-01-23 14:04:42 +0000

    Implemented moving enemies

--- scripts/SceneControllers/MainSceneController.cs
+++ scripts/SceneControllers/MainSceneController.cs
@@ -10,13 +10,16 @@ public class MainSceneController : Node2D
 	private Node2D node_actors;
 	private Node2D node_enemies;
 
+	private Position2D node_enemiesLeftBounds;
+	private Position2D node_enemiesRightBounds;
+
 	private Player node_player;
 
 	private Position2D node_playerSpawnPosition;
 
 	private Position2D node_alienSpawnPosition_left;
 	private Position2D node_alienSpawnPosition_right;
-
+	private Node2D node_aliens;
 	private Timer node_alienSpawnTimer;
 
 	private Node2D node_background;
@@ -26,6 +29,10 @@ public class MainSceneController : Node2D
 
 
 	#region Properties
+
+	[Export] public float EnemyMoveSpeed { get; set; } = 1f;
+	[Export] public int EnemyMoveDirection { get; set; } = 1;
+	[Export] public float EnemyMoveDistance { get; set; } = 8f;
 
 	private int _lives = 3;
 
@@ -60,7 +67,9 @@ public class MainSceneController : Node2D
 	[Export] private PackedScene m_packedScene_redAlien;
 	[Export] private PackedScene m_packedScene_purpleAlien;
 
-	private int m_enemiesLeft = 0;
+	private int m_enemyCount = 0;
+
+	private float m_enemyMoveTimer = 0f;
 
 	#endregion // Fields
 
@@ -75,13 +84,16 @@ public class MainSceneController : Node2D
 		node_actors = GetNode<Node2D>("Actors");
 		node_enemies = node_actors.GetNode<Node2D>("Enemies");
 
+		node_enemiesLeftBounds = node_enemies.GetNode<Position2D>("Left");
+		node_enemiesRightBounds = node_enemies.GetNode<Position2D>("Right");
+
 		node_player = node_actors.GetNode<Player>("Player");
 
 		node_playerSpawnPosition = GetNode<Position2D>("PlayerSpawnPosition");
 
 		node_alienSpawnPosition_left = GetNode<Position2D>("AlienSpawnPositionLeft");
 		node_alienSpawnPosition_right = GetNode<Position2D>("AlienSpawnPositionRight");
-
+		node_aliens = node_actors.GetNode<Node2D>("Aliens");
 		node_alienSpawnTimer = GetNode<Timer>("AlienSpawnTimer");
 
 		node_background = GetNode<Node2D>("Background");
@@ -98,19 +110,25 @@ public class MainSceneController : Node2D
 		{
 			PinkHead pinkHead = node_enemies.GetChildOrNull<PinkHead>(i);
 			GreyHead greyHead = node_enemies.GetChildOrNull<GreyHead>(i);
-			
+
 			if (pinkHead != null)
 			{
 				pinkHead.Connect(nameof(PinkHead.OnHit), this, nameof(OnPinkHeadHit));
-				m_enemiesLeft++;
+				m_enemyCount++;
 			}
 			else if (greyHead != null)
 			{
 				greyHead.Connect(nameof(GreyHead.OnHit), this, nameof(OnGreyHeadHit));
-				m_enemiesLeft++;
+				m_enemyCount++;
 			}
 		}
+
+		m_previousEnemyCount = m_enemyCount;
+
+		RecalculateEnemyBounds();
 	}
+
+	private int m_previousEnemyCount = 0;
 
 	public override void _PhysicsProcess(float delta)
 	{
@@ -129,6 +147,42 @@ public class MainSceneController : Node2D
 		position.y = Mathf.Wrap(position.y, -384, 0);
 
 		node_background.Position = position;
+
+		if (m_enemyCount > 0)
+		{
+			if (m_previousEnemyCount != m_enemyCount)
+			{
+				RecalculateEnemyBounds();
+			}
+
+			Vector2 left = node_enemiesLeftBounds.GlobalPosition;
+			Vector2 right = node_enemiesRightBounds.GlobalPosition;
+
+			m_enemyMoveTimer += delta;
+			if (m_enemyMoveTimer >= EnemyMoveSpeed)
+			{
+				m_enemyMoveTimer = 0;
+				if (EnemyMoveDirection == -1 && left.x <= 0)
+				{
+					node_enemies.Translate(new Vector2(0, 16));
+					EnemyMoveSpeed -= 0.1f;
+					EnemyMoveSpeed = Mathf.Clamp(EnemyMoveSpeed, 0.1f, Mathf.Inf);
+					EnemyMoveDirection = 1;
+				}
+				else if (EnemyMoveDirection == 1 && right.x >= 256)
+				{
+					node_enemies.Translate(new Vector2(0, 16));
+					EnemyMoveSpeed -= 0.1f;
+					EnemyMoveSpeed = Mathf.Clamp(EnemyMoveSpeed, 0.1f, Mathf.Inf);
+					EnemyMoveDirection = -1;
+				}
+				else
+				{
+					Vector2 translation = new Vector2(EnemyMoveDistance * EnemyMoveDirection, 0);
+					node_enemies.Position += translation;
+				}
+			}
+		}
 	}
 
 	#endregion // Godot methods
@@ -136,6 +190,47 @@ public class MainSceneController : Node2D
 
 
 	#region Private methods
+
+	private void RecalculateEnemyBounds()
+	{
+		float left = 256;
+		float right = 0;
+		float bottom = 0;
+
+		for(int i = 0; i < node_enemies.GetChildCount(); i++)
+		{
+			PinkHead pinkHead = node_enemies.GetChildOrNull<PinkHead>(i);
+			GreyHead greyHead = node_enemies.GetChildOrNull<GreyHead>(i);
+
+			if (pinkHead == null && greyHead == null) continue;
+
+			Node2D node = node_enemies.GetChild<Node2D>(i);
+
+			if (i == 0)
+			{
+				left = node.GlobalPosition.x;
+				right = node.GlobalPosition.x;
+				bottom = node.GlobalPosition.y;
+			}
+
+			if (node.GlobalPosition.x < left)
+			{
+				left = node.GlobalPosition.x;
+			}
+			else if (node.GlobalPosition.x > right)
+			{
+				right = node.GlobalPosition.x;
+			}
+
+			if (node.GlobalPosition.y > bottom)
+			{
+				bottom = node.GlobalPosition.y;
+			}
+		}
+
+		node_enemiesLeftBounds.GlobalPosition = new Vector2(left - 32, bottom + 32);
+		node_enemiesRightBounds.GlobalPosition = new Vector2(right + 32, bottom + 32);
+	}
 
 	private void OnPlayerHit()
 	{
@@ -153,8 +248,9 @@ public class MainSceneController : Node2D
 	private void OnPinkHeadHit()
 	{
 		Score += 100;
-		m_enemiesLeft--;
-		if (m_enemiesLeft <= 0)
+		m_previousEnemyCount = m_enemyCount;
+		m_enemyCount--;
+		if (m_enemyCount <= 0)
 		{
 			GetTree().ReloadCurrentScene();
 		}
@@ -163,8 +259,9 @@ public class MainSceneController : Node2D
 	private void OnGreyHeadHit()
 	{
 		Score += 250;
-		m_enemiesLeft--;
-		if (m_enemiesLeft <= 0)
+		m_previousEnemyCount = m_enemyCount;
+		m_enemyCount--;
+		if (m_enemyCount <= 0)
 		{
 			GetTree().ReloadCurrentScene();
 		}
@@ -182,7 +279,7 @@ public class MainSceneController : Node2D
 		bool left = rng.RandiRange(0, 1) == 0 ? false : true;
 
 		RedAlien alien = m_packedScene_redAlien.Instance<RedAlien>();
-		node_enemies.AddChild(alien);
+		node_aliens.AddChild(alien);
 		alien.Position = left ? node_alienSpawnPosition_left.Position : node_alienSpawnPosition_right.Position;
 		float speed = 64;
 		alien.Velocity = left ? Vector2.Right * speed : Vector2.Left * speed;
